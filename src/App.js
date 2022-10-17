d5d4fbc371ba55ad2aa7f112bf23b96dc250020e
Anton Kosar <anton.kosar@sobre.pl>
2022-10-17 11:16:10 +0200

    feat: add react-pure-component component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import ReactComponent from "./components/ReactComponent";
 import ReactCreateElement from "./components/ReactCreateElement";
+import ReactPureComponent from "./components/ReactPureComponent";
 
 function App() {
   return (
     <>
       <ReactCreateElement />
       <ReactComponent />
+      <ReactPureComponent />
     </>
   );
 }
