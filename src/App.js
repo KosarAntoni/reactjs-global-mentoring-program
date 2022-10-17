db62b499d12a1cd6d4b720ee770aa68047b50acb
Anton Kosar <anton.kosar@sobre.pl>
2022-10-17 11:01:28 +0200

    feat: add react-component component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import ReactComponent from "./components/ReactComponent";
 import ReactCreateElement from "./components/ReactCreateElement";
 
 function App() {
   return (
     <>
       <ReactCreateElement />
+      <ReactComponent />
     </>
   );
 }
