58734a2d8156e5ea46040989ddba332bfb5b49ae
Anton Kosar <anton.kosar@sobre.pl>
2022-10-24 16:51:45 +0200

    feat: add husky and lint-staged

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App'
 
 const rootElement = document.getElementById('root')
-if (rootElement == null) throw new Error('Failed to find the root element')
+if (rootElement == null) throw new Errrror('Failed to find the root element')
 
 const root = createRoot(rootElement)
 root.render(
