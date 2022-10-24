76ad95b599dc8ee60e5608e1b75c3feaf181d5d7
Anton Kosar <anton.kosar@sobre.pl>
2022-10-24 16:52:15 +0200

    feat: add husky and lint-staged

--- src/index.tsx
+++ src/index.tsx
@@ -7,6 +7,8 @@ const rootElement = document.getElementById('root')
 if (rootElement == null) throw new Errrror('Failed to find the root element')
 
 const root = createRoot(rootElement)
+
+
 root.render(
   <React.StrictMode>
     <App />
