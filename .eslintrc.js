f2959c91dc517ca5a4f5515dbb200296514fa0e1
Anton Kosar <anton.kosar@sobre.pl>
2022-10-24 16:37:43 +0200

    feat: add eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,29 +1,19 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true,
+    es2021: true
   },
-  extends: ["plugin:react/recommended", "standard-with-typescript"],
+  extends: ['plugin:react/recommended', 'standard-with-typescript'],
   settings: {
     react: {
-      version: "detect",
-    },
+      version: 'detect'
+    }
   },
   parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module",
-    project: "./tsconfig.json",
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json'
   },
-  plugins: ["react"],
-  rules: {},
-  // overrides: [
-  //   {
-  //     files: ["*.js", "*.jsx"],
-  //     rules: {
-  //       quotes: [0, "single"],
-  //       "comma-dangle": [0, "never"],
-  //       semi: [0],
-  //     },
-  //   },
-  // ],
-};
+  plugins: ['react'],
+  rules: {}
+}
