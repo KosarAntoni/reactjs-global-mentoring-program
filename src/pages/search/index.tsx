592dff26116ce7a2300c57aad02a346db4297d02
Anton Kosar <anton.kosar@sobre.pl>
2022-12-26 19:11:14 +0100

    fix: redirect to search page

--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -2,13 +2,17 @@ import React from 'react'
 import type { NextPage } from 'next'
 import Head from 'next/head'
 
+import Layout from 'templates/Layout'
+
 const IndexPage: NextPage = () => {
     return (
       <>
         <Head>
-          <title>Index</title>
+          <title>Search</title>
           <link href='/favicon.ico' rel='icon' />
         </Head>
+
+        <Layout />
       </>
     )
   }
