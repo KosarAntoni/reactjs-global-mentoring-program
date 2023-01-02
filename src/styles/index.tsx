2d4120f20d0b029ad23024673efc48c9bd3970e0
Anton Kosar <anton.kosar@sobre.pl>
2023-01-02 18:48:09 +0100

    feat: add global styles

--- src/styles/index.tsx
+++ src/styles/index.tsx
@@ -1,19 +1,22 @@
 import React from 'react'
 import { css, Global } from '@emotion/react'
 
-export const globalStyles = (
-  <Global
-    styles={css`
-      html {
-        font-size: 62.5%;
-      }
+import { normalize } from './normalize'
+import { variables } from './variables'
 
-      body {
-        font-family: var(--primary-font);
-        font-size: 1.6rem;
-        color: var(--primary-color);
-        background-color: var(--page-background);
-      }
-    `}
-  />
-)
+const global = css`
+  ${normalize}
+  ${variables}
+  html {
+    font-size: 62.5%;
+  }
+
+  body {
+    font-family: var(--primary-font);
+    font-size: 1.6rem;
+    color: var(--primary-color);
+    background-color: var(--page-background);
+  }
+`
+
+export const globalStyles = <Global styles={global} />
